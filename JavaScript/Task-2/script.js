8742d4a3da15e0d2e4f6330cf8358e17354ca311
Naveen Krish <98112231+Naveen-McDev@users.noreply.github.com>
2022-03-14 11:07:04 +0530

    Add files via upload

---
+++ JavaScript/Task-2/script.js
@@ -0,0 +1,15 @@
+/* Question : Print the characters in a string separated by line.. */
+
+let str = "NATURE";
+
+//the characters in a string can be separated by split()
+
+// The split() method divides a String into an ordered list of substrings, puts these substrings into an array, and returns the array.
+
+// The join() method creates and returns a new string by concatenating all of the elements in an array
+
+let output = str.split("").join("\n")
+
+
+
+console.log(output);
